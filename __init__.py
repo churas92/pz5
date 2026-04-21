425d5fae366d8315dd2437c42ed4498020465c39
churas92 <vova.varlamov1997@gmail.com>
2026-04-21 11:17:00 +0300

    Delete __init__.py

--- __init__.py
+++
@@ -1,9 +0,0 @@
-
-__version__ = "1.0.0"
-__author__ = "вова варламов"
-
-from .models.customer import Customer
-from .models.store import Store
-from .models.order import Order
-
-__all__ = ["Customer", "Store", "Order"]
